5bb529ba63863a73696a53d111fdee5454a7a0aa
Ahmad Salameh <ahmadsalameh.dev@gmail.com>
2022-02-19 09:37:33 +0200

    Implement address bar

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { TiWeatherWindy } from "react-icons/ti";
+import { GrLocation } from "react-icons/gr";
+import Flags from "country-flag-icons/react/3x2";
 
 import "./header.css";
 
 const Header = () => {
+  const [country, setCountry] = useState("JO");
+  const [address, setAddress] = useState("Amman, Jordan");
+
+  const location = useRef();
+
+  const Flag = Flags[country];
+
+  const focus = () => {
+    location.current.focus();
+  };
   return (
     <header>
       <div className="logo">
@@ -24,7 +36,19 @@ const Header = () => {
           </li>
         </ul>
       </nav>
-      <div className="search"></div>
+      <div className="search">
+        <form onSubmit={(e) => e.preventDefault()}>
+          <div className="form-overlay" onClick={focus}></div>
+          <span className="flag">
+            <Flag />
+          </span>
+          <input type="text" value={address} ref={location} />
+          <span className="location">
+            <GrLocation />
+          </span>
+          <div className="form-background"></div>
+        </form>
+      </div>
     </header>
   );
 };
