6e9d5b0a4a4c1cf44562b2ced75d9aaca8bdea88
Ahmad Salameh <ahmadsalameh.dev@gmail.com>
2022-02-25 11:42:16 +0300

    Implement min time to load

--- src/app.js
+++ src/app.js
@@ -9,11 +9,19 @@ const App = () => {
   const [weather, setWeather] = useState({});
 
   useEffect(() => {
+    const startingTime = new Date().getTime() / 1000;
     fetch(
       "https://api.weatherapi.com/v1/forecast.json?key=41685b31f7104bf597f114815222002&q=Amman&days=10&aqi=no&alerts=no"
     )
       .then((res) => res.json())
-      .then((d) => setWeather(d));
+      .then((d) => {
+        const endTime = new Date().getTime() / 1000;
+        if (endTime - startingTime < 3)
+          setTimeout(() => {
+            setWeather(d);
+          }, 2000);
+        else setWeather(d);
+      });
   }, []);
 
   return (
