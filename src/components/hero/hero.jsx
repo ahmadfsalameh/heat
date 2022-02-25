517f6559d447118f48e3127dcd9a2ebb2ff1ec86
Ahmad Salameh <ahmadsalameh.dev@gmail.com>
2022-02-25 09:59:37 +0300

    Make data dynamic

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,33 +1,55 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../header/header";
-import { FaCloudMoon } from "react-icons/fa";
-import { BsArrowDownCircle } from "react-icons/bs";
+import { RiMouseLine } from "react-icons/ri";
 
-import heroImage from "../../assets/images/hero.jpg";
+import heroImage from "../../assets/images/hero1.jpg";
+import weatherJSON from "../../constants/weather";
 import "./hero.css";
 
 const Hero = () => {
-  const [weather, setWeather] = useState({
-    now: {
-      description: "cloudy",
-      celsius: 18,
-      fahrenheit: 50,
-    },
-  });
+  const [weather, setWeather] = useState({});
   const [unit, setUnit] = useState("celsius");
+
+  useEffect(() => {
+    fetch(
+      "https://api.weatherapi.com/v1/forecast.json?key=41685b31f7104bf597f114815222002&q=Amman&days=10&aqi=no&alerts=no"
+    )
+      .then((res) => res.json())
+      .then((d) => setWeather(d));
+  }, []);
 
   const changeUnit = ({ target }) => {
     setUnit(target.dataset.unit);
   };
+
+  const getIcon = (code, isDay) => {
+    const weatherObj = weatherJSON.find((e) => e.code === code);
+
+    let IconName;
+
+    if (isDay) IconName = weatherObj.icon[0];
+    else IconName = weatherObj.icon[1];
+
+    return <IconName className="weather-icon" />;
+  };
+
+  if (!Object.keys(weather).length) return null;
+
+  const { current } = weather;
   return (
     <section className="hero">
       <img src={heroImage} className="cover" />
       <Header />
       <div className="weather-status">
-        <FaCloudMoon className="weather-icon" />
+        {getIcon(current.condition.code, current.isDay)}
         <p>
-          <span>{weather.now.description}</span>
-          <span>{weather.now[unit]}º</span>
+          <span>{weather.current.condition.text}</span>
+          <span>
+            {unit === "celsius"
+              ? weather.current.temp_c
+              : Math.round(weather.current.temp_f)}
+            º
+          </span>
           <div className="units">
             <button
               data-active={unit == "celsius" && "1"}
@@ -46,7 +68,7 @@ const Hero = () => {
           </div>
         </p>
         <button className="scroll-down">
-          Scroll down <BsArrowDownCircle />
+          Scroll down <RiMouseLine />
         </button>
       </div>
     </section>
